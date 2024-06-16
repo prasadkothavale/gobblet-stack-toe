3b2e7a2fcafafbd10a399b43729ca660d121dba0
Prasad Kothavale <prasadkothavale@yahoo.co.in>
2024-06-16 18:22:48 +0100

    Add game engine functional test

---
+++ gobblet-stack-toe-engine/src/game-engine-functional.test.ts
@@ -0,0 +1,48 @@
+import GameEngine from "./game-engine";
+import { GameConfig, Game, GameStatus, Player } from "./interface";
+
+describe("Game engine functional test", () => {
+    const ge = GameEngine;
+
+    it("can play random games with all valid moves", () => {
+        const totalGames = 10 ** 3;
+        const gc: GameConfig = {
+            boardSize: 4,
+            gobbletSize: 3,
+            gobbletsPerSize: 3
+        }
+        let whiteWins = 0;
+        let blackWins = 0;
+        let repetitionDraw = 0;
+        let doubleDraw = 0;
+
+        for (let i: number = 0; i < totalGames; i++) {
+            const game: Game = ge.createGame(gc);
+            while(game.state.status === GameStatus.LIVE) {
+                const moves: string[] = GameEngine.getValidMovesNotations(game);
+                const randomMove: string = moves[Math.round(Math.random() * (moves.length - 1))];
+                ge.performMoveFromNotation(game, randomMove);
+            }
+            
+            switch(game.state.status) {
+                case GameStatus.DOUBLE_DRAW: 
+                    doubleDraw++;
+                    break;
+                case GameStatus.REPETITION_DRAW:
+                    repetitionDraw++;
+                    break;
+                case GameStatus.END:
+                    if (game.state.winner === Player.WHITE) {
+                        whiteWins++;
+                    } else {
+                        blackWins++;
+                    }
+                    break;
+                default:
+                    throw new Error("Invalid game status: " + game.state.status);
+            }
+        }
+
+        console.log(`Games execution status:\n=======================\nwhite wins: ${whiteWins}\nblack wins: ${blackWins}\ndraw by repetition: ${repetitionDraw}\ndouble draw: ${doubleDraw}\n========================`);
+    });
+});
